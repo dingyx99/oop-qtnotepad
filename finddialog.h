8089f528f3004306724fb9b7dfe67558dc5c29f4
dingyx99 <dingyuxiao99@nwafu.edu.cn>
2019-07-03 10:09:06 +0800

    find dialog

--- finddialog.h
+++ finddialog.h
@@ -1,11 +1,39 @@
 #ifndef FINDDIALOG_H
 #define FINDDIALOG_H
 
+#include<QDialog>
+#include<QCheckBox>
+#include<QLabel>
+#include<QLineEdit>
+#include<QPushButton>
 
-class findDialog
+class QCheckBox;
+class QLabel;
+class QLineEdit;
+class QPushButton;
+
+class findDialog: public QDialog
 {
+    Q_OBJECT
 public:
-    findDialog();
+    findDialog(QWidget * parent = nullptr);
+
+signals:
+    void findNext(const QString & str, Qt::CaseSensitivity cs);
+    void findPrevious(const QString & str, Qt::CaseSensitivity cs);
+
+private slots:
+    void findClicked();
+    void enableFindButton(const QString & text);
+
+private:
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QCheckBox *caseCheckBox;
+    QCheckBox *backwardCheckBox;
+    QPushButton *findButton;
+    QPushButton *closeButton;
 };
+
 
 #endif // FINDDIALOG_H
